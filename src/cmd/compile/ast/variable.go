417d45610e88beda9017cf2b9bc3cf17e8624dfc
yuyang <756445638@qq.com>
2018-10-16 16:00:16 +0800

    save files

--- src/cmd/compile/ast/variable.go
+++ src/cmd/compile/ast/variable.go
@@ -3,20 +3,22 @@ package ast
 import "gitee.com/yuyang-fine/lucy/src/cmd/compile/jvm/cg"
 
 type Variable struct {
-	IsBuildIn              bool
-	IsGlobal               bool
-	IsFunctionParameter    bool
-	IsReturn               bool
-	BeenCaptured           int
-	Used                   bool   // use as right value
-	AccessFlags            uint16 // public private or protected
-	Pos                    *Pos
-	DefaultValueExpression *Expression
-	Name                   string
-	Type                   *Type
-	LocalValOffset         uint16 // offset in stack frame
-	JvmDescriptor          string // jvm
-	Comment                string
+	IsBuildIn               bool
+	IsGlobal                bool
+	IsFunctionParameter     bool
+	IsReturn                bool
+	BeenCaptured            int
+	BeenCapturedAsLeftValue int
+	BeenCapturedAsLeftRight int
+	Used                    bool   // use as right value
+	AccessFlags             uint16 // public private or protected
+	Pos                     *Pos
+	DefaultValueExpression  *Expression
+	Name                    string
+	Type                    *Type
+	LocalValOffset          uint16 // offset in stack frame
+	JvmDescriptor           string // jvm
+	Comment                 string
 }
 
 func (v *Variable) isPublic() bool {
