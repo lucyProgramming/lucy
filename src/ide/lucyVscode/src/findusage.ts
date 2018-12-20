4b598e12a6f782aaa203cecaa42696bdd6bcc5ea
yu <756445638@qq.com>
2018-12-20 15:56:59 +0800

    save files

--- src/ide/lucyVscode/src/findusage.ts
+++ src/ide/lucyVscode/src/findusage.ts
@@ -19,7 +19,8 @@ module.exports = class GoReferenceProvider implements vscode.ReferenceProvider {
             var res  = syncHttpRequest("GET" , u);
             var usages = JSON.parse(res.getBody());
             if (!usages) {
-                return null;
+                reject("not found usages");
+                return;
             }
             console.log(usages);
             var items = new Array();
@@ -34,39 +35,3 @@ module.exports = class GoReferenceProvider implements vscode.ReferenceProvider {
         });
     }
 };
-
-
-
-// 'use strict';
-// // The module 'vscode' contains the VS Code extensibility API
-// // Import the module and reference it with the alias vscode in your code below
-// import * as vscode from 'vscode';
-
-// const querystring = require('querystring');
-// const syncHttpRequest = require('sync-request');
-
-// module.exports = class GoReferenceProvider implements vscode.ReferenceProvider {
-//     public provideReferences(
-//         document: vscode.TextDocument, position: vscode.Position,
-//         options: { includeDeclaration: boolean }, token: vscode.CancellationToken):
-//         Thenable<vscode.Location[]> {
-//         var u = "http://localhost:2018/ide/findUsage?file=" + querystring.escape(document.fileName) + "&line=" + 
-//             position.line + "&column=" + position.character; 
-//         console.log(u);
-//         var res  = syncHttpRequest("GET" , u);
-//         var usages = JSON.parse(res.getBody());
-//         if (!usages) {
-//             return null;
-//         }
-//         console.log(usages);
-//         var items = new Array();
-//         for(var i = 0 ; i < usages.length ; i ++ ) {
-//             let v = usages[i];
-//             var uri2 = vscode.Uri.file(v.pos.filename);
-//             var position2 = new vscode.Position(v.pos.endLine,v.pos.endColumnOffset);
-//             var location =  new vscode.Location(uri2, position2);
-//             items[i] = location; 
-//         } 
-//         return items; 
-//     }
-// };
--- src/ide/lucyVscode/src/goto_definition.ts
+++ src/ide/lucyVscode/src/goto_definition.ts
@@ -23,6 +23,7 @@ module.exports = class GoDefinitionProvider implements vscode.DefinitionProvider
             });
             let definition = JSON.parse(res.getBody());
             if (!definition) {
+                reject("definition not found");
                 return ;
             }
             let uri2 =  vscode.Uri.file(path.normalize(definition.filename)) ; 
@@ -34,38 +35,3 @@ module.exports = class GoDefinitionProvider implements vscode.DefinitionProvider
 
 
 
-
-// 'use strict';
-// // The module 'vscode' contains the VS Code extensibility API
-// // Import the module and reference it with the alias vscode in your code below
-// import * as vscode from 'vscode';
-
-// const path = require('path');
-// const querystring = require('querystring');
-// const syncHttpRequest = require('sync-request');
-
-
-// module.exports = class GoDefinitionProvider implements vscode.DefinitionProvider {
-//     public provideDefinition(
-//         document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken):
-//         Thenable<vscode.Location> {
-        
-//         var u = "http://localhost:2018/ide/gotoDefinition?file=" + querystring.escape(document.fileName) + "&line=" + 
-//             position.line + "&column=" + position.character ; 
-//         console.log(u);
-//         let buffer = document.getText();
-//         var res  = syncHttpRequest("POST" , u ,  {
-//             "body": buffer,
-//         });
-
-//         let definition = JSON.parse(res.getBody());
-//         if (!definition) {
-//             return null;
-//         }
-//         let uri2 =  vscode.Uri.file(path.normalize(definition.filename)) ; 
-//         let position2 = new vscode.Position(definition.endLine, definition.endColumnOffset);
-//         //fs.unlink(bufferFileName);
-//         return new vscode.Location(uri2, position2);
-//         }
-//     };
-// }
